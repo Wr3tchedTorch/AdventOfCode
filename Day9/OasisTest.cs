189cb3e831ed306431fb13756677eddfcea5ea46
Eric Moura <ericmoura511@gmail.com>
2024-03-10 21:29:19 -0300

    Day 9 challenge complete

--- Day9/OasisTest.cs
+++ Day9/OasisTest.cs
@@ -8,4 +8,11 @@ public class OasisTest
         Oasis myOasis = new Oasis(@"C:\Users\ericm\Desktop\AdventOfCode\Day9\files\first_example.txt");
         Assert.Equal(114, myOasis.GetExtrapolatedSum());
     }
+
+    [Fact]
+    public void TestPuzzleInput()
+    {
+        Oasis myOasis = new Oasis(@"C:\Users\ericm\Desktop\AdventOfCode\Day9\files\puzzle_input.txt");
+        Assert.Equal(1647269739, myOasis.GetExtrapolatedSum());
+    }
 }
